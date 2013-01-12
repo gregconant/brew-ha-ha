db9be8e364ef257e11f5d1a3c9af9498d52e1eda
Greg Conant <greg.conant@gmail.com>
2013-01-12 14:22:06 -0500

    gc - initial commit of basic project created by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrewHaHaRuby::Application.config.secret_token = '49bc54c88a5983851a21662cd25de9d0204bc6965c53ebfea22d9b83c564724a55968a9d23097d4617a715e42aea24a9b3d8b76fdcad28dcf3d94bf3ed501415'
